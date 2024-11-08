7e5c1884686fecdb377b860f43028f871ebd970d
bigthor123 <wiktor.wojciechowski@thinkingzone.pl>
2024-11-08 13:55:43 +0100

    Update zadanie8.py

--- zadanie8.py
+++ zadanie8.py
@@ -3,3 +3,10 @@ licznik_nie = 0
 while not pada:
   print("Nie pada")
   odpowiedz - input("czy pada? (tak/nie)")
+  if odpowiedz == 'tak':
+    print('powiedziales nie: ', licznik_nie , 'razy')
+    pada = True
+  elif odpowiedz == 'nie':
+    licznik_nie += 1
+  else:
+    print("to wyjdz na dwor i sie dowiedz")
